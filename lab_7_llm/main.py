13291fcef07278c0cc3f544172eebf88eaa5efeb
polsha24 <polsha24@yandex.ru>
2026-01-24 22:09:48 +0300

    mark 4

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -6,6 +6,11 @@ Working with Large Language Models.
 
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
 from typing import Iterable, Sequence
+import pandas as pd
+from datasets import load_dataset
+from core_utils.llm.raw_data_importer import AbstractRawDataImporter
+from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor
+from core_utils.llm.time_decorator import report_time
 
 
 class RawDataImporter(AbstractRawDataImporter):
@@ -21,6 +26,17 @@ class RawDataImporter(AbstractRawDataImporter):
         Raises:
             TypeError: In case of downloaded dataset is not pd.DataFrame
         """
+        dataset = load_dataset(self._hf_name, split="validation")
+
+        if not hasattr(dataset, "to_pandas"):
+            raise TypeError("Downloaded dataset is not a pandas DataFrame")
+        
+        df = dataset.to_pandas()
+
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("Downloaded dataset is not a pandas DataFrame")
+        
+        self._raw_data = df
 
 
 class RawDataPreprocessor(AbstractRawDataPreprocessor):
@@ -35,6 +51,29 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
         Returns:
             dict: Dataset key properties
         """
+        df = self._raw_data.copy()
+
+        num_samples = len(df)
+        num_columns = len(df.columns)
+        num_duplicates = df.duplicated().sum()
+        num_empty_rows = df.isnull().all(axis=1).sum()
+
+        text_col = 'text'
+        df_non_empty = df.dropna(subset=[text_col])
+
+        lengths = df_non_empty[text_col].str.len()
+        min_len = int(lengths.min())
+        max_len = int(lengths.max())
+
+        return {
+            "dataset_number_of_samples": int(num_samples),
+            "dataset_columns": int(num_columns),
+            "dataset_duplicates": int(num_duplicates),
+            "dataset_empty_rows": int(num_empty_rows),
+            "dataset_sample_min_len": min_len,
+            "dataset_sample_max_len": max_len,
+        }
+
 
     @report_time
     def transform(self) -> None:
@@ -43,127 +82,127 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
         """
 
 
-class TaskDataset(Dataset):
-    """
-    A class that converts pd.DataFrame to Dataset and works with it.
-    """
+# class TaskDataset(Dataset):
+#     """
+#     A class that converts pd.DataFrame to Dataset and works with it.
+#     """
 
-    def __init__(self, data: pd.DataFrame) -> None:
-        """
-        Initialize an instance of TaskDataset.
+#     def __init__(self, data: pd.DataFrame) -> None:
+#         """
+#         Initialize an instance of TaskDataset.
 
-        Args:
-            data (pandas.DataFrame): Original data
-        """
+#         Args:
+#             data (pandas.DataFrame): Original data
+#         """
 
-    def __len__(self) -> int:
-        """
-        Return the number of items in the dataset.
+#     def __len__(self) -> int:
+#         """
+#         Return the number of items in the dataset.
 
-        Returns:
-            int: The number of items in the dataset
-        """
+#         Returns:
+#             int: The number of items in the dataset
+#         """
 
-    def __getitem__(self, index: int) -> tuple[str, ...]:
-        """
-        Retrieve an item from the dataset by index.
+#     def __getitem__(self, index: int) -> tuple[str, ...]:
+#         """
+#         Retrieve an item from the dataset by index.
 
-        Args:
-            index (int): Index of sample in dataset
+#         Args:
+#             index (int): Index of sample in dataset
 
-        Returns:
-            tuple[str, ...]: The item to be received
-        """
+#         Returns:
+#             tuple[str, ...]: The item to be received
+#         """
 
-    @property
-    def data(self) -> DataFrame:
-        """
-        Property with access to preprocessed DataFrame.
+#     @property
+#     def data(self) -> DataFrame:
+#         """
+#         Property with access to preprocessed DataFrame.
 
-        Returns:
-            pandas.DataFrame: Preprocessed DataFrame
-        """
-
-
-class LLMPipeline(AbstractLLMPipeline):
-    """
-    A class that initializes a model, analyzes its properties and infers it.
-    """
-
-    def __init__(
-        self, model_name: str, dataset: TaskDataset, max_length: int, batch_size: int, device: str
-    ) -> None:
-        """
-        Initialize an instance.
-
-        Args:
-            model_name (str): The name of the pre-trained model
-            dataset (TaskDataset): The dataset used
-            max_length (int): The maximum length of generated sequence
-            batch_size (int): The size of the batch inside DataLoader
-            device (str): The device for inference
-        """
-
-    def analyze_model(self) -> dict:
-        """
-        Analyze model computing properties.
-
-        Returns:
-            dict: Properties of a model
-        """
-
-    @report_time
-    def infer_sample(self, sample: tuple[str, ...]) -> str | None:
-        """
-        Infer model on a single sample.
-
-        Args:
-            sample (tuple[str, ...]): The given sample for inference with model
-
-        Returns:
-            str | None: A prediction
-        """
-
-    @report_time
-    def infer_dataset(self) -> pd.DataFrame:
-        """
-        Infer model on a whole dataset.
-
-        Returns:
-            pd.DataFrame: Data with predictions
-        """
-
-    @torch.no_grad()
-    def _infer_batch(self, sample_batch: Sequence[tuple[str, ...]]) -> list[str]:
-        """
-        Infer model on a single batch.
-
-        Args:
-            sample_batch (Sequence[tuple[str, ...]]): Batch to infer the model
-
-        Returns:
-            list[str]: Model predictions as strings
-        """
+#         Returns:
+#             pandas.DataFrame: Preprocessed DataFrame
+#         """
 
 
-class TaskEvaluator(AbstractTaskEvaluator):
-    """
-    A class that compares prediction quality using the specified metric.
-    """
+# class LLMPipeline(AbstractLLMPipeline):
+#     """
+#     A class that initializes a model, analyzes its properties and infers it.
+#     """
 
-    def __init__(self, data_path: Path, metrics: Iterable[Metrics]) -> None:
-        """
-        Initialize an instance of Evaluator.
+#     def __init__(
+#         self, model_name: str, dataset: TaskDataset, max_length: int, batch_size: int, device: str
+#     ) -> None:
+#         """
+#         Initialize an instance.
 
-        Args:
-            data_path (pathlib.Path): Path to predictions
-            metrics (Iterable[Metrics]): List of metrics to check
-        """
+#         Args:
+#             model_name (str): The name of the pre-trained model
+#             dataset (TaskDataset): The dataset used
+#             max_length (int): The maximum length of generated sequence
+#             batch_size (int): The size of the batch inside DataLoader
+#             device (str): The device for inference
+#         """
 
-    def run(self) -> dict:
-        """
-        Evaluate the predictions against the references using the specified metric.
+#     def analyze_model(self) -> dict:
+#         """
+#         Analyze model computing properties.
 
-        Returns:
-            dict: A dictionary containing information about the calculated metric
-        """
+#         Returns:
+#             dict: Properties of a model
+#         """
+
+#     @report_time
+#     def infer_sample(self, sample: tuple[str, ...]) -> str | None:
+#         """
+#         Infer model on a single sample.
+
+#         Args:
+#             sample (tuple[str, ...]): The given sample for inference with model
+
+#         Returns:
+#             str | None: A prediction
+#         """
+
+#     @report_time
+#     def infer_dataset(self) -> pd.DataFrame:
+#         """
+#         Infer model on a whole dataset.
+
+#         Returns:
+#             pd.DataFrame: Data with predictions
+#         """
+
+#     @torch.no_grad()
+#     def _infer_batch(self, sample_batch: Sequence[tuple[str, ...]]) -> list[str]:
+#         """
+#         Infer model on a single batch.
+
+#         Args:
+#             sample_batch (Sequence[tuple[str, ...]]): Batch to infer the model
+
+#         Returns:
+#             list[str]: Model predictions as strings
+#         """
+
+
+# class TaskEvaluator(AbstractTaskEvaluator):
+#     """
+#     A class that compares prediction quality using the specified metric.
+#     """
+
+#     def __init__(self, data_path: Path, metrics: Iterable[Metrics]) -> None:
+#         """
+#         Initialize an instance of Evaluator.
+
+#         Args:
+#             data_path (pathlib.Path): Path to predictions
+#             metrics (Iterable[Metrics]): List of metrics to check
+#         """
+
+#     def run(self) -> dict:
+#         """
+#         Evaluate the predictions against the references using the specified metric.
+
+#         Returns:
+#             dict: A dictionary containing information about the calculated metric
+#         """
--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -3,6 +3,10 @@ Starter for demonstration of laboratory work.
 """
 
 # pylint: disable=too-many-locals, undefined-variable, unused-import
+from unittest import result
+from core_utils.llm.time_decorator import report_time
+from lab_7_llm.main import RawDataImporter
+from lab_7_llm.main import RawDataPreprocessor
 
 
 @report_time
@@ -10,8 +14,19 @@ def main() -> None:
     """
     Run the translation pipeline.
     """
-    result = None
+    importer = RawDataImporter('papluca/language-identification')
+    importer.obtain()
+    result = importer.raw_data
     assert result is not None, "Demo does not work correctly"
+
+    print(result.head())
+
+    preprocessor = RawDataPreprocessor(importer.raw_data)
+    analysis = preprocessor.analyze()
+
+    print('Dataset analysis results:')
+    for key, value in analysis.items():
+        print(f'{key}: {value}')
 
 
 if __name__ == "__main__":
