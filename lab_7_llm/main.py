204ad17995ebbba61913c6d7d60f196e1a13b1ce
polsha24 <polsha24@yandex.ru>
2026-01-27 17:04:12 +0300

    fixed code style errors & unittests

--- lab_7_llm/main.py
+++ lab_7_llm/main.py
@@ -4,11 +4,22 @@ Laboratory work.
 Working with Large Language Models.
 """
 
+from pathlib import Path
+
 # pylint: disable=too-few-public-methods, undefined-variable, too-many-arguments, super-init-not-called
+from typing import Iterable, Sequence
+
 import pandas as pd
+import torch
 from datasets import load_dataset
+from pandas import DataFrame
+from torch.utils.data import Dataset
+
+from core_utils.llm.llm_pipeline import AbstractLLMPipeline
+from core_utils.llm.metrics import Metrics
 from core_utils.llm.raw_data_importer import AbstractRawDataImporter
 from core_utils.llm.raw_data_preprocessor import AbstractRawDataPreprocessor
+from core_utils.llm.task_evaluator import AbstractTaskEvaluator
 from core_utils.llm.time_decorator import report_time
 
 
@@ -76,4 +87,130 @@ class RawDataPreprocessor(AbstractRawDataPreprocessor):
     def transform(self) -> None:
         """
         Apply preprocessing transformations to the raw dataset.
+        """
+
+
+class TaskDataset(Dataset):
+    """
+    A class that converts pd.DataFrame to Dataset and works with it.
+    """
+
+    def __init__(self, data: pd.DataFrame) -> None:
+        """
+        Initialize an instance of TaskDataset.
+
+        Args:
+            data (pandas.DataFrame): Original data
+        """
+
+    def __len__(self) -> int:
+        """
+        Return the number of items in the dataset.
+
+        Returns:
+            int: The number of items in the dataset
+        """
+
+    def __getitem__(self, index: int) -> tuple[str, ...]:
+        """
+        Retrieve an item from the dataset by index.
+
+        Args:
+            index (int): Index of sample in dataset
+
+        Returns:
+            tuple[str, ...]: The item to be received
+        """
+
+    @property
+    def data(self) -> DataFrame:
+        """
+        Property with access to preprocessed DataFrame.
+
+        Returns:
+            pandas.DataFrame: Preprocessed DataFrame
+        """
+
+
+class LLMPipeline(AbstractLLMPipeline):
+    """
+    A class that initializes a model, analyzes its properties and infers it.
+    """
+
+    def __init__(
+        self, model_name: str, dataset: TaskDataset, max_length: int, batch_size: int, device: str
+    ) -> None:
+        """
+        Initialize an instance.
+
+        Args:
+            model_name (str): The name of the pre-trained model
+            dataset (TaskDataset): The dataset used
+            max_length (int): The maximum length of generated sequence
+            batch_size (int): The size of the batch inside DataLoader
+            device (str): The device for inference
+        """
+
+    def analyze_model(self) -> dict:
+        """
+        Analyze model computing properties.
+
+        Returns:
+            dict: Properties of a model
+        """
+
+    @report_time
+    def infer_sample(self, sample: tuple[str, ...]) -> str | None:
+        """
+        Infer model on a single sample.
+
+        Args:
+            sample (tuple[str, ...]): The given sample for inference with model
+
+        Returns:
+            str | None: A prediction
+        """
+
+    @report_time
+    def infer_dataset(self) -> pd.DataFrame:
+        """
+        Infer model on a whole dataset.
+
+        Returns:
+            pd.DataFrame: Data with predictions
+        """
+
+    @torch.no_grad()
+    def _infer_batch(self, sample_batch: Sequence[tuple[str, ...]]) -> list[str]:
+        """
+        Infer model on a single batch.
+
+        Args:
+            sample_batch (Sequence[tuple[str, ...]]): Batch to infer the model
+
+        Returns:
+            list[str]: Model predictions as strings
+        """
+
+
+class TaskEvaluator(AbstractTaskEvaluator):
+    """
+    A class that compares prediction quality using the specified metric.
+    """
+
+    def __init__(self, data_path: Path, metrics: Iterable[Metrics]) -> None:
+        """
+        Initialize an instance of Evaluator.
+
+        Args:
+            data_path (pathlib.Path): Path to predictions
+            metrics (Iterable[Metrics]): List of metrics to check
+        """
+
+    def run(self) -> dict:
+        """
+        Evaluate the predictions against the references using the specified metric.
+
+        Returns:
+            dict: A dictionary containing information about the calculated metric
         """
--- lab_7_llm/start.py
+++ lab_7_llm/start.py
@@ -9,6 +9,26 @@ from types import SimpleNamespace
 # pylint: disable=too-many-locals, undefined-variable, unused-import
 from lab_7_llm.main import RawDataImporter, RawDataPreprocessor, report_time
 
+try:
+    from transformers import (
+        AutoModelForSequenceClassification,
+        AutoTokenizer,
+        XLMRobertaForSequenceClassification
+    )
+except ImportError:
+    print('Library "transformers" not installed. Failed to import.')
+
+try:
+    from torchinfo import summary  # type: ignore
+except ImportError:
+    print('Library "torchinfo" not installed. Failed to import.')
+
+try:
+    import torch
+except ImportError:
+    print('Library "torch" not installed. Failed to import.')
+
+
 SETTINGS_PATH = Path(__file__).resolve().with_name("settings.json")
 
 @report_time
@@ -23,6 +43,68 @@ def main() -> None:
 
     preprocessor = RawDataPreprocessor(importer.raw_data)
     analysis = preprocessor.analyze()
+
+    tokenizer = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
+
+    text = "KFC заработал в Нижнем под новым брендом"
+    tokens = tokenizer(text, return_tensors="pt")
+
+    print(tokens.keys())
+
+    raw_tokens = tokenizer.convert_ids_to_tokens(tokens["input_ids"].tolist()[0])
+    print(raw_tokens)
+
+    print(tokens["input_ids"].tolist()[0])
+
+    model = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
+
+    print(model)
+
+    model.eval()
+
+    with torch.no_grad():
+        output = model(**tokens)
+
+    print(output.logits)
+    print(output.logits.shape)
+
+    predictions = torch.argmax(output.logits).item()
+
+    print(predictions)
+
+    labels = model.config.id2label
+    print(labels[predictions])
+
+
+    model = XLMRobertaForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
+    print(type(model))
+
+    config = model.config
+
+    print(config)
+
+    embeddings_length = config.max_position_embeddings
+
+    ids = torch.ones(1, embeddings_length, dtype=torch.long)
+
+    tokens = {"input_ids": ids, "attention_mask": ids}
+
+    result = summary(model, input_data=tokens, device="cpu", verbose=0)
+
+    print(result)
+
+    inp_shape = result.input_size
+    print(f"input_shape:\n{inp_shape}\n")
+
+    n_params = result.total_params
+    print(f"num_trainable_params:\n{n_params}\n")
+
+    summary_list = result.summary_list
+    print(f"summary_list:\n{summary_list}\n")
+
+    total_p_bytes = result.total_param_bytes
+    print(f"total_param_bytes:\n{total_p_bytes}\n")
+
 
     result = analysis
     assert result is not None, "Demo does not work correctly"
